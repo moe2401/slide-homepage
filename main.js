94a17c3cdbed881f0a51a74c1cf0bec98c1f7ca5
moe <moe.aoki0124@gmail.com>
2022-09-05 20:53:10 +1000

    first iterarion; not yet working correctly.

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
-let i = 0;
-let images = [];
-let time = 5000;
+let sliderImages = document.querySelectorAll(".slide"),
+arrowLeft = document.querySelector("#arrow-left"),
+arrowRight = document.querySelector("#arrow-right"),
+current = 0;
 
-//image list
-images[0] = "img/asian-food.jpg";
-images[1] = "img/coffee.jpg";
-images[2] = "img/hamburger.jpg";
-images[3] = "img/italian-food.jpg";
-
-
-//change image
-function changeImg(){
-    document.getElementById("main-img").style.backgroundImage = "url(" + images[i] +")";
-
-    if(i < images.length - 1){
-        i++;
-    } else{
-        i = 0;
+//Clear all images
+function reset(){
+    for(let i = 0; i < sliderImages.length; i++){
+        sliderImages[i].style.display = "none";
     }
-
-    setTimeout("changeImg()", time);
 }
 
-// window.onload = changeImg();
+//Init slider
+function startSlide(){
+    reset();
+    sliderImages[0].style.display = "block";
+}
+
+//Show prev
+function slideLeft(){
+    reset();
+    sliderImages[current - 1].style.display = "block";
+    current--;
+}
+
+//Show next
+function slideRight(){
+    reset();
+    sliderImages[current + 1].style.display = "block";
+    current++;
+}
+
+//Left arrow click
+arrowLeft.addEventListener("click", function(){
+    if(current === 0){
+        current = sliderImages.length;
+    }
+    slideLeft();
+});
+
+//Right arrow click
+arrowRight.addEventListener("click", function(){
+    if(current === sliderImages.length - 1){
+        current = -1
+    }
+    slideRight();
+});
+
+
+startSlide();
